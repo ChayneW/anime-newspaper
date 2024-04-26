912b2853db338de035a11202d807fe1e067be222
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-04-26 16:58:01 -0400

    added key in suspense.

--- app/page.js
+++ app/page.js
@@ -13,23 +13,25 @@ export default function Home() {
         
         <div className='grid xl:grid-cols-3'>
           {newsPapers.map((paper, _index) => (
-            <Suspense fallback={<LoadingCard/>}>
-              <div key={_index} className={`relative grid justify-center py-4`}>
-                <h1 className='text-center py-2 text-white'>{paper.name}</h1>
-                <div className='relative w-[380px] h-[500px] p-4'>
-                  <Link
-                    href={`/${paper.page}`}
-                  >
-                    <Image
-                      className='rounded-xl'
-                      src={`/${paper.gif}`}
-                      fill
-                      objectFit='cover'
-                    />
-                  </Link>
+            <>
+              <Suspense key={_index} fallback={<LoadingCard/>}>
+                <div key={_index} className={`relative grid justify-center py-4`}>
+                  <h1 className='text-center py-2 text-white'>{paper.name}</h1>
+                  <div className='relative w-[380px] h-[500px] p-4'>
+                    <Link
+                      href={`/${paper.page}`}
+                    >
+                      <Image
+                        className='rounded-xl'
+                        src={`/${paper.gif}`}
+                        fill
+                        objectFit='cover'
+                      />
+                    </Link>
+                  </div>
                 </div>
-              </div>
-            </Suspense>
+              </Suspense>
+            </>
           ))}
           {/* <LoadingCard/> */}
         </div>
