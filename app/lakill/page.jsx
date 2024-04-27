5e54fee5df992d6800ba1b1c8e06d0319566374b
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-04-27 14:12:38 -0400

    updated new gif for kill la kill.

---
+++ app/lakill/page.jsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+
+const KillPage = () => {
+  return (
+    <div className='max-container min-h-screen bg-white py-5'>
+        kill la kill
+        
+      
+    </div>
+  )
+}
+
+export default KillPage
