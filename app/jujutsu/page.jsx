8ef70998b6cf7aa69cd8e06f3054b40032b9532d
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-04-28 15:21:16 -0400

    updated title text.

--- app/jujutsu/page.jsx
+++ app/jujutsu/page.jsx
@@ -7,7 +7,7 @@ const JujutsuPage = () => {
         <div className='grid w-full h-full gap-10'>
             {/* calmity */}
             <div className='grid justify-center gap-2'>
-                <h1 className='font-archivo font-bold leading-none tracking-wide text-center max-xl:text-3xl xl:text-[200px]'>カラミティ</h1>
+                <h1 className='font-archivo font-bold leading-none tracking-wide text-center max-xl:text-[3rem] xl:text-[200px]'>カラミティ</h1>
                 <h1 className='font-noto font-bold max-xl:text-md xl:text-xl text-center px-5 text-wrap'>呪いの計画的な攻撃により、パワーバランスは変化し始める。かつて渋谷の街であった戦場がむき出しになる一方で、柔術部隊は復興への道を急ぐ。</h1>
             </div>
 
@@ -50,23 +50,29 @@ const JujutsuPage = () => {
             {/* text */}
             <div className='relative'>
 
-                <div className='grid absolute z-10 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 '>
-                    {/* <h1 className='font-noto font-semibold text-red-600 text-6xl'>渋谷事件</h1> */}
-                    <h1 className='font-noto font-bold text-red-600 text-6xl'>SHIBUYA</h1>
-                    <h1 className='font-noto font-semibold text-red-600 text-6xl'>INCIDENT</h1>
-                    <p className='font-noto font-semibold text-red-600 text-2xl'> アーク</p>
-                    {/* <div className="w-16 h-16 bg-gray-300 rounded-xl rotate-12"></div> */}
+                <div className='grid absolute z-10 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+                    <h1 className='font-noto font-bold text-red-600 max-lg:text-3xl lg:text-6xl'>SHIBUYA</h1>
+                    <h1 className='font-noto font-semibold text-red-600 max-lg:text-3xl lg:text-6xl'>INCIDENT</h1>
+                    <p className='font-noto font-semibold text-red-600 max-xl:text-lg xl:text-2xl'> アーク</p>
                 </div>
 
                 {/* main */}
                 <div className='relative z-0 w-[100%] h-[300px]'>
                     <Image
-                        className=''
+                        className='max-xl:hidden xl:block'
                         src={'/jujutsu/shibuya-arc-black.gif'}
                         alt='gif'
                         fill
                         // style={{objectFit: 'contain'}}
                         style={{objectFit: 'cover'}}
+                    />
+                    <Image
+                        className='max-xl:block xl:hidden bg-black'
+                        src={'/jujutsu/shibuya-arc-black.gif'}
+                        alt='gif'
+                        fill
+                        // style={{objectFit: 'cover'}}
+                        style={{objectFit: 'contain'}}
                     />
                 </div>
             </div>
@@ -109,7 +115,7 @@ const JujutsuPage = () => {
 
             <div className='grid justify-center'>
                 <h1 className='font-noto font-bold text-xl text-center py-2 text-wrap'>With an orchestrated onslaught from the curses, the pendulum of power starts to shift, as the warzone of what once was the city of Shibuya lays bare while the Jujutsu Forces rush to recover in what is considered a ...</h1>
-                <h1 className='font-archivo font-bold text-center leading-none tracking-wide max-xl:text-2xl xl:text-[150px]'>CALMITY</h1>
+                <h1 className='font-archivo font-bold text-center leading-none tracking-wide max-xl:text-[3rem] xl:text-[150px]'>CALMITY</h1>
             </div>
 
         </div>
