074cf4c101d556d4bf12f52e9954a8e7310cc77e
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-05-03 16:48:05 -0400

    changed link button

--- components/PaperCard.jsx
+++ components/PaperCard.jsx
@@ -17,22 +17,26 @@ const PaperCard = ({name, gif, page}) => {
 
             <div className='relative z-0 w-[380px] h-[500px] p-4 max-md:p-6'>
                 <div className='absolute z-10 top-[10%] right-5 rounded-lg bg-gray-900 opacity-70 p-2' >
-                    <Image
-                        className=''
-                        src={'/arrow-diagonal.svg'}
-                        width={30}
-                        height={30}
-                        loading='lazy'
-                    />
+                    <Link
+                        href={`/${page}`}
+                    >
+                        <Image
+                            className=''
+                            src={'/arrow-diagonal.svg'}
+                            width={30}
+                            height={30}
+                            loading='lazy'
+                        />
+                    </Link>
                 </div>
 
                 <div className='absolute z-10 bottom-[10%] left-5 bg-gray-400 rounded-lg px-2'>
                     <h1 className='font-bitter text-center font-bold py-2 text-white'>{name}</h1>
                 </div>
                 
-                <Link
+                {/* <Link
                     href={`/${page}`}
-                >
+                > */}
                     <Image
                         className='rounded-xl'
                         src={`/${gif}`}
@@ -42,7 +46,7 @@ const PaperCard = ({name, gif, page}) => {
                         onLoad={handleImageLoad}
                     />
                     {imageLoaded ? null : <div className="absolute inset-0 flex justify-center items-center"><LoadingCard/></div>}
-                </Link>
+                {/* </Link> */}
             </div>
         </div>
 
