2a5ee23e24d7609c1926ddb039e7d0e2d7e869d0
Chaynes-MacBookPro <ChayneW-MacBookPro.local>
2024-09-14 14:41:25 -0400

    added analytics next

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/Navbar'
 import Footer from '@/components/Footer'
+import { Analytics } from "@vercel/analytics/react"
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +18,7 @@ export default function RootLayout({ children }) {
       <body className='main-page'>
         <Navbar/>
         {children}
+        <Analytics />
         {/* <Footer/> */}
       </body>
     </html>
